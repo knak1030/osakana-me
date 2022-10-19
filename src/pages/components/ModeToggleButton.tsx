5189dbfac27013e3ef04aa0380b5d857f46ea179
長加奈子 <>
2022-10-19 22:26:09 +0200

    color mode 切り替えボタンでパネルがcloseするのを修正

--- src/pages/components/ModeToggleButton.tsx
+++ src/pages/components/ModeToggleButton.tsx
@@ -4,8 +4,13 @@ import { Button, useColorMode } from '@chakra-ui/react'
 
 const ModeToggleButton = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <Button aria-label='toggle color mode' onClick={toggleColorMode} bg={'transparent'} justifyContent={'flex-end'}>
+    <Button
+      aria-label='toggle color mode'
+      onClick={(e) => {toggleColorMode(); e.stopPropagation()}}
+      bg={'transparent'} justifyContent={'flex-end'}
+    >
       {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
     </Button>
   )
